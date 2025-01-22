b74770bea41f7939f956c99faefba594a4a06b38
AndyHung955587CS <955587@pdsb.net>
2025-01-22 02:37:15 -0500

    Update ivtdriver.py

--- ivtdriver.py
+++ ivtdriver.py
@@ -3,7 +3,7 @@
 # Date of submission: 1/22/2025
 # Program: Ivt driver, Final Project: The IVT Theorem
 # Description: Driver to run the ivt class, returns an estimate using the ivt theorem
-# of where the 0 might be based on a polynomial and two guesses (x1, x2) as to where the 0 might be 
+# of where the 0 might be based on a polynomial and two guesses (x1, x2)
 # VARIABLE DICTIONARY:
 #   P (Polynomial.Polynomial) = coefficients (numbers in an array) of a polynomial
 #   x1 (float) = first guess  
